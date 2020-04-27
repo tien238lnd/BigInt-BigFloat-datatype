bd748243262af95af52d3536f3a496845174da71
tien238lnd <39857648+tien238lnd@users.noreply.github.com>
2020-04-27 23:46:16 +0700

    Update QInt.h

--- QInt.h
+++ QInt.h
@@ -1,36 +1,56 @@
-﻿#pragma once
+#pragma once
 #include <iostream>
-#include <string>
+//#include "DecString.h"
 
 class QInt
 {
-	char Q[16];
+	static const int NUMBYTES = 16;
+	static const int NUMBITS = 128;
+	char bytes[NUMBYTES];
 public:
-	QInt& operator=(QInt& des);
-	QInt& operator=(std::string str);
-	
+
+	// Hiền
+
+	QInt();
+	QInt(const QInt& src);
+	QInt(const char* srcStr);
+
+	QInt& operator=(QInt& src);
+	QInt& operator=(std::string srcStr);	// str này ở hệ 10 thôi phải ko?
+											// có nhận str kiểu như "0x12345ABC" hay "0b101110001" ko? CÓ
+
+private:
+	void setBit(char i, bool b);
+	bool getBit(char i) const;
+	void convertTo2sComplement();
+	void DectoBin(bool* &binStr) const;		// chỉ dùng để phục vụ hàm xuất
+	void BintoDec(bool* &binStr);			// chỉ dùng để phục vụ hàm nhập từ chuỗi "0b010101"
+	void DectoHex(char* &hexStr) const;
+	void HextoDec(char* &hexStr);
+
+
 friend std::istream& operator<<(std::istream& istr, QInt& qi);
-friend std::ostream& operator>>(std::ostream& istr, QInt& qi);
-	
-friend bool* DectoBin(QInt x);
-friend QInt BintoDec(bool* bit);
+friend std::ostream& operator>>(std::ostream& istr, const QInt& qi);
 
-friend char* DectoHex(QInt x);
-friend QInt HextoDec(char* hex);
+//////////////////////////////////////////////////
+// Hiếu
 
-friend QInt operator+(QInt &x, QInt &y);
-friend QInt operator-(QInt &x, QInt &y);
-friend QInt operator*(QInt &x, QInt &y);
-friend QInt operator/(QInt &x, QInt &y);
+friend QInt operator+(const QInt &x, const QInt &y);
+friend QInt operator-(const QInt &x, const QInt &y);
+friend QInt operator*(const QInt &x, const QInt &y);	// thuật toán Booth?
+friend QInt operator/(const QInt &x, const QInt &y);	// KHÓ
 
 // có viết mấy hàm +=, ++ đồ ko?	=> Ko
 
-friend bool operator==(QInt &x, QInt &y);
-friend bool operator!=(QInt &x, QInt &y);
-friend bool operator<(QInt &x, QInt &y);
-friend bool operator>(QInt &x, QInt &y);
-friend bool operator<=(QInt &x, QInt &y);
-friend bool operator>=(QInt &x, QInt &y);
+friend bool operator==(const QInt &x, const QInt &y);
+friend bool operator!=(const QInt &x, const QInt &y);
+friend bool operator<(const QInt &x, const QInt &y);	// chạy từ most significant bit tới
+friend bool operator>(const QInt &x, const QInt &y);
+friend bool operator<=(const QInt &x, const QInt &y);
+friend bool operator>=(const QInt &x, const QInt &y);
+
+//////////////////////////////////////////////////
+// Tiến
 
 friend QInt operator&(QInt &x, QInt &y);
 friend QInt operator|(QInt &x, QInt &y);
@@ -39,9 +59,7 @@ friend QInt operator~(QInt &x);
 
 friend QInt operator>>(QInt &x, int i);
 friend QInt operator<<(QInt &x, int i);
+
 friend QInt rol(QInt &x, int i);
 friend QInt ror(QInt &x, int i);
 };
-
-char* BintoHex(bool* bit);
-bool* HextoBin(char* hex);
