acf059f7e08e6903cfc156d6aa48037947a2236e
PLHieu <hieuflong@gmail.com>
2020-04-28 08:33:37 +0700

    Fix loi setbit, getbit (nham lan 8 voi 16)

--- QInt.cpp
+++ QInt.cpp
@@ -1,18 +1,22 @@
 #include "QInt.h"
 
 
+// mình biểu diễn ngược, least thì ở bên trái, most thì ở bên phải
+// ví dụ byte i cũng chạy từ 0 bên trái sang 7 bên phải
+//  muốn set bit thứ 5 thì phải dịch bit cuối của 00000001 sang trái 3 nấc.
+// vậy tức là | ( 1<< (8 - i%8) 
 void QInt::setBit(char i, bool b)
 {
 	if (b == 1)
-		this->bytes[i / 16] = this->bytes[i / 16] | (1 << i % 16);
+		this->bytes[i / 8] = this->bytes[i / 8] | (1 <<(8 - i % 8));
 	else
-		this->bytes[i / 16] = this->bytes[i / 16] & ~(1 << i % 16);
+		this->bytes[i / 8] = this->bytes[i / 8] & ~(1 << (8-i % 8));
 }
 
 
 bool QInt::getBit(char i) const
 {
-	return (this->bytes[i / 16] >> i % 16) & 1;
+	return  (this->bytes[i / 8] >> (8- i % 8) ) & 1;
 }
 
 
